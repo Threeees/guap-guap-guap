e4ea13741611fd4c79f5944bf7b28e56fa0e2d30
Kathryn Wicks <ktwicks1@gmail.com>
2016-08-04 19:00:43 -0400

    adds leaderboard

--- web_service.py
+++ web_service.py
@@ -3,18 +3,19 @@ import requests
 import datetime
 import schedule
 import time
+import json
 from firebase import firebase
 
-firebase = firebase.FirebaseApplication('https://guap-6f88c.firebaseio.com')
+firebase = firebase.FirebaseApplication('https://guap-test.firebaseio.com/')
 parameters = {'api_key': 'owtDCkSy4nZ7G7J1dSEa', 'order':'desc', 'rows':4}
-FIREBASE_SECRET = 'Z2UD52Yp7TYKzkDD8GC7G5PgAPSGGKsdbpPsc3sL'
+FIREBASE_SECRET = 'N1mFEGq6SSFn1GE6srl4aKgzyxCqMTGNNcVvGtDc'
 stock_tickers = firebase.get('/bets', None, params = {'shallow':'true', 'auth':FIREBASE_SECRET})
 
 class Stock():
 	_ticker = "GUAP"
 	_prev_close = 0.00
 	_today_close = 0.00
-	current_date = datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%d')
+	current_date = datetime.datetime.strftime(datetime.datetime.now()-datetime.timedelta(1),'%Y-%m-%d')
 
 	def __init__ (self, ticker):
 		self._ticker = ticker
@@ -45,25 +46,25 @@ class Stock():
 	   
 
 	def getWinningPool(self):
-		#winningBettors = []
+		winningBettors = []
 		winningPool = 0
-		#losingBettors = []
+		losingBettors = []
 		losingPool = 0
 		bettors = self.getBettors()
 		if len(bettors) > 0 and bettors != None:
 			for index, bettor in enumerate(bettors):
 				if (((bettor['direction'] == 'up') and (self.getDirection() == True)) or ((bettor['direction'] == 'down') and (self.getDirection() == False))):
 					winningPool += float(bettor['amount'])
-					#winningBettors.append(bettor)
+					winningBettors.append(bettor)
 					bettor['isWinner'] = True
 					firebase.patch('/bets/'+self._ticker+'/'+self.current_date+'/bettors/'+str(index),
 									data = {'isWinner': True}, params = {'auth':FIREBASE_SECRET})
 				else:
 					losingPool += float(bettor['amount'])
-					#losingBettors.append(bettor)
+					losingBettors.append(bettor)
 					bettor['isWinner'] = False
 					firebase.patch('/bets/'+self._ticker+'/'+self.current_date+'/bettors/'+str(index),
-									data = {'isWinner': False}, params = {'auth':FIREBASE_SECRET})
+					 				data = {'isWinner': False}, params = {'auth':FIREBASE_SECRET})
 			print (losingPool, winningPool)
 			if winningPool == 0:
 				pass
@@ -73,16 +74,38 @@ class Stock():
 				for bettor in bettors:
 					if bettor['isWinner'] == True:
 						amt = float(bettor['amount']) * (1 + payoutRatio)
+						if (firebase.get('/users/'+bettor['user']+'/Pending Bets',"amt_won", params = {'auth':FIREBASE_SECRET}) != None):
+							amt_won = (firebase.get('/users/'+bettor['user']+'/Pending Bets',"amt_won", params = {'auth':FIREBASE_SECRET}))
+							firebase.put('/users/'+bettor['user'], data={'amt_won': amt_won+ amt}, name = 'Pending Bets',params = {'auth':FIREBASE_SECRET})
+							print(bettor['user']+' is already a winner, amount won: '+ str(amt_won))
+						else:
+							firebase.put('/users/'+bettor['user'], data = {'amt_won':amt}, name = 'Pending Bets', params = {'auth':FIREBASE_SECRET})
+							print(bettor['user']+'\'s first win of the day, amount won: '+ str(amt))
 						balance = float(firebase.get('/users/'+bettor['user']+"/Wallet", "deposit" , params = {'auth':FIREBASE_SECRET}))
 						deposit = balance + amt
+						
 						firebase.patch('/users/'+bettor['user']+"/Wallet",
 									data={'deposit': str(deposit)},
 									params = {'auth':FIREBASE_SECRET})
 					else:
+						print(bettor['user']+" lost on "+ self._ticker)
 						pass
 		else:
 			pass
-
+def get_leaderboard():
+	leaderboard = []
+	users = firebase.get('/users', None, params = {'auth':FIREBASE_SECRET})
+	json.dumps(users)
+	#print (users)
+	for key, value in users.items():
+		try:
+			leaderboard.append(dict({'user':str(key), 'amt_won': (value['Pending Bets']['amt_won'])}))
+		except:
+			pass
+	
+	leaderboard = sorted(leaderboard, key=lambda k: k['amt_won'], reverse = True)
+	print(leaderboard)
+	firebase.put('/leaderboard/', data = leaderboard, name = 'Leaderboard', params = {'auth': FIREBASE_SECRET})
 def sort():
 	if len(stock_tickers) > 0:
 		for ticker in stock_tickers:
@@ -92,6 +115,7 @@ def sort():
 	else:
 		pass
 
+get_leaderboard()
 schedule.every().day.at("16:16").do(sort)
 while True:
     schedule.run_pending()
